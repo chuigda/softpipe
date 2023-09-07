c8a20a07aae4b2607a1dc7d58cd424a3a165e0f3
ICEYSELF <icey@icey.tech>
2023-09-07 23:07:40 +0800

    rundos

--- native/rundos.bat
+++ native/rundos.bat
@@ -1,11 +1,6 @@
 gcc softpipe.c -c -g -o softpipe.o -Wall -Wextra
-pause
-
 gcc sputnikd.c -c -g -o sputnik.o -Wall -Wextra
-pause
-
 gcc demo.c -c -g -o demo.o -Wall -Wextra
-pause
 
 gcc softpipe.o sputnik.o demo.o -lm -o demo.exe
 demo.exe
